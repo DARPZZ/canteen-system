1d6b08d6ac3c8397a143972bdb92247776c578c6
rasmustofth <104558794+DARPZZ@users.noreply.github.com>
2023-04-13 12:15:21 +0200

    Admin and custommer login screen

--- canteen-system2/src/main/java/com/example/canteensystem2/LogIn.java
+++ canteen-system2/src/main/java/com/example/canteensystem2/LogIn.java
@@ -5,6 +5,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -14,20 +16,38 @@ import java.util.ArrayList;
 
 public class LogIn extends Application
 {
-Button adminLoginB = new Button();
-Button customerLoginB = new Button();
-
-
+Button adminLoginB = new Button("Admin login");
+Button customerLoginB = new Button("Custommer login");
 
     @Override
     public void start(Stage stage) throws IOException
     {
-
         BorderPane borderPane = new BorderPane();
         AnchorPane anchorPane = new AnchorPane();
         Scene scene = new Scene(borderPane, 1280, 768);
+        Label welcome = new Label("Welcome to Canteen ");
+        Label choice = new Label("Please select what you would like to log in as");
         borderPane.setCenter(anchorPane);
-        anchorPane.getChildren().addAll(adminLoginB, customerLoginB);
+        anchorPane.getChildren().addAll(adminLoginB, customerLoginB,welcome,choice);
+
+
+        adminLoginB.setLayoutY(300);
+        adminLoginB.setPrefWidth(150);
+        adminLoginB.setPrefHeight(20);
+        adminLoginB.setLayoutX(scene.getWidth()/2 -(adminLoginB.getPrefWidth()/2) );
+
+        customerLoginB.setLayoutX(adminLoginB.getLayoutX());
+        customerLoginB.setLayoutY(adminLoginB.getLayoutY()+50);
+        customerLoginB.setPrefHeight(adminLoginB.getPrefHeight());
+        customerLoginB.setPrefWidth(adminLoginB.getPrefWidth());
+
+        welcome.setLayoutX(adminLoginB.getLayoutX()+20);
+        welcome.setLayoutY(adminLoginB.getLayoutY()-100);
+
+        choice.setLayoutX(adminLoginB.getLayoutX()-40);
+        choice.setLayoutY(adminLoginB.getLayoutY()-20);
+
+
 
         //region sceneShift
         customerLoginB.setOnAction(new EventHandler<ActionEvent>()
