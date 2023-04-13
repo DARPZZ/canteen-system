9a6b0c8a4dcb0e0ddc79a1285ba81c65cf2ef86f
rasmustofth <104558794+DARPZZ@users.noreply.github.com>
2023-04-13 12:16:43 +0200

    Scene makeing

---
+++ canteen-system2/src/main/java/com/example/canteensystem2/CustommerLogin.java
@@ -0,0 +1,12 @@
+package com.example.canteensystem2;
+
+import javafx.scene.Scene;
+
+public class CustommerLogin
+{
+
+    private Scene scene;
+    public Scene custommerLogin() {
+        return scene;
+    }
+}
